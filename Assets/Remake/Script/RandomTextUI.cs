49d889cdc846ba055691909dffb387c0c1a093e9
Ivenaccip <45603061+Ivenaccip@users.noreply.github.com>
2019-01-07 00:47:35 -0600

    Final Random Numbers
    Finaly i make the randon numbers with 10 differents options, but i found a bug that happend when the button Start/other is pushed a lot of time, the Si_B1 won't detect whether it's correct or not.

--- Assets/Remake/Script/RandomTextUI.cs
+++ Assets/Remake/Script/RandomTextUI.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RandomTextUI : MonoBehaviour {
 
@@ -10,33 +11,47 @@ public class RandomTextUI : MonoBehaviour {
 	public GameObject NumText3;
 	public GameObject DSOOobject;
 
+	public GameObject Operations;
+
 	public Button No1;
 
 	public Button No2;
+
+	public Button No3;
 	public GameObject NoBut1;
 	public GameObject NoBut2;
+
+	public GameObject NoBut3;
+	public string Next;
+
+
 
 	//When the others buttons will be touched, the start n' other will deactivate
 
 	public bool DSOO;
+
+	public bool change;
 	
 	public int AleNum;
 	
 	public void randomGenerator()
 	{
-		AleNum = Random.Range(1,4);
+		AleNum = Random.Range(1,11);
 		
 
 		if (AleNum == 1)
 		{
 			DSOO = false;
-			NumText1.GetComponent<Text> ().text = "Chocolate";
-			NumText2.GetComponent<Text> ().text = "Cereza";
-			NumText3.GetComponent<Text> ().text = "Cono";
+			NumText1.GetComponent<Text> ().text = "1";
+			NumText2.GetComponent<Text> ().text = "2";
+			NumText3.GetComponent<Text> ().text = "3";
+			Operations.GetComponent<Text> ().text = "1 + 1";	
 			Button BN_1 = No1.GetComponent<Button>();
 			BN_1.onClick.AddListener(NoB1);
 			Button BN_2 = No2.GetComponent<Button>();
-			BN_2.onClick.AddListener(ONoB1);
+			BN_2.onClick.AddListener(Si_B1);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(NoB3);
 			
 
 		}
@@ -44,25 +59,136 @@ public class RandomTextUI : MonoBehaviour {
 		if (AleNum == 2)
 		{
 			DSOO = false;
-			NumText1.GetComponent<Text> ().text = "Fresa";
-			NumText2.GetComponent<Text> ().text = "Frambuesa";
-			NumText3.GetComponent<Text> ().text = "Barquillo";
+			NumText1.GetComponent<Text> ().text = "3";
+			NumText2.GetComponent<Text> ().text = "4";
+			NumText3.GetComponent<Text> ().text = "5";
+			Operations.GetComponent<Text> ().text = "2 + 3";
 			Button BN_1 = No1.GetComponent<Button>();
-			BN_1.onClick.AddListener(NoB2);
+			BN_1.onClick.AddListener(NoB1);
 			Button BN_2 = No2.GetComponent<Button>();
-			BN_2.onClick.AddListener(ONoB2);
+			BN_2.onClick.AddListener(NoB2);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(Si_B1);
 		}
 
 		if (AleNum == 3)
 		{
 			DSOO = false;
-			NumText1.GetComponent<Text> ().text = "Vainilla";
-			NumText2.GetComponent<Text> ().text = "Verde";
-			NumText3.GetComponent<Text> ().text = "Vaso";
+			NumText1.GetComponent<Text> ().text = "5";
+			NumText2.GetComponent<Text> ().text = "6";
+			NumText3.GetComponent<Text> ().text = "7";
+			Operations.GetComponent<Text> ().text = "3 + 2";
 			Button BN_1 = No1.GetComponent<Button>();
-			BN_1.onClick.AddListener(NoB3);
+			BN_1.onClick.AddListener(Si_B1);
 			Button BN_2 = No2.GetComponent<Button>();
-			BN_2.onClick.AddListener(ONoB2);
+			BN_2.onClick.AddListener(NoB2);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(NoB3);
+		}
+
+		if (AleNum == 4)
+		{
+			DSOO = false;
+			NumText1.GetComponent<Text> ().text = "7";
+			NumText2.GetComponent<Text> ().text = "8";
+			NumText3.GetComponent<Text> ().text = "9";
+			Operations.GetComponent<Text> ().text = "4 + 4";
+			Button BN_1 = No1.GetComponent<Button>();
+			BN_1.onClick.AddListener(NoB1);
+			Button BN_2 = No2.GetComponent<Button>();
+			BN_2.onClick.AddListener(Si_B1);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(NoB3);
+		}
+
+		if (AleNum == 5)
+		{
+			DSOO = false;
+			NumText1.GetComponent<Text> ().text = "9";
+			NumText2.GetComponent<Text> ().text = "1";
+			NumText3.GetComponent<Text> ().text = "3";
+			Operations.GetComponent<Text> ().text = "1 + 2";
+			Button BN_1 = No1.GetComponent<Button>();
+			BN_1.onClick.AddListener(NoB1);
+			Button BN_2 = No2.GetComponent<Button>();
+			BN_2.onClick.AddListener(NoB2);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(Si_B1);
+		}
+
+		if (AleNum == 6)
+		{
+			DSOO = false;
+			NumText1.GetComponent<Text> ().text = "3";
+			NumText2.GetComponent<Text> ().text = "5";
+			NumText3.GetComponent<Text> ().text = "7";
+			Operations.GetComponent<Text> ().text = "1 + 1 + 1";
+			Button BN_1 = No1.GetComponent<Button>();
+			BN_1.onClick.AddListener(Si_B1);
+			Button BN_2 = No2.GetComponent<Button>();
+			BN_2.onClick.AddListener(NoB2);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(NoB3);
+		}
+
+		if (AleNum == 7)
+		{
+			DSOO = false;
+			NumText1.GetComponent<Text> ().text = "7";
+			NumText2.GetComponent<Text> ().text = "9";
+			NumText3.GetComponent<Text> ().text = "3";
+			Operations.GetComponent<Text> ().text = "7 + 1 + 1";
+			Button BN_1 = No1.GetComponent<Button>();
+			BN_1.onClick.AddListener(NoB1);
+			Button BN_2 = No2.GetComponent<Button>();
+			BN_2.onClick.AddListener(Si_B1);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(NoB3);
+		}
+
+		if (AleNum == 8)
+		{
+			DSOO = false;
+			NumText1.GetComponent<Text> ().text = "3";
+			NumText2.GetComponent<Text> ().text = "6";
+			NumText3.GetComponent<Text> ().text = "9";
+			Operations.GetComponent<Text> ().text = "3 + 3 + 3";
+			Button BN_1 = No1.GetComponent<Button>();
+			BN_1.onClick.AddListener(NoB1);
+			Button BN_2 = No2.GetComponent<Button>();
+			BN_2.onClick.AddListener(NoB2);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(Si_B1);
+		}
+
+		if (AleNum == 9)
+		{
+			DSOO = false;
+			NumText1.GetComponent<Text> ().text = "9";
+			NumText2.GetComponent<Text> ().text = "4";
+			NumText3.GetComponent<Text> ().text = "8";
+			Operations.GetComponent<Text> ().text = "9 + 0";
+			Button BN_1 = No1.GetComponent<Button>();
+			BN_1.onClick.AddListener(Si_B1);
+			Button BN_2 = No2.GetComponent<Button>();
+			BN_2.onClick.AddListener(NoB2);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(NoB3);
+		}
+
+		if (AleNum == 10)
+		{
+			DSOO = false;
+			NumText1.GetComponent<Text> ().text = "1";
+			NumText2.GetComponent<Text> ().text = "5";
+			NumText3.GetComponent<Text> ().text = "6";
+			Operations.GetComponent<Text> ().text = "1 + 0";
+			Button BN_1 = No1.GetComponent<Button>();
+			BN_1.onClick.AddListener(Si_B1);
+			Button BN_2 = No2.GetComponent<Button>();
+			BN_2.onClick.AddListener(NoB2);
+			Button BN_3 = No3.GetComponent<Button>();
+			BN_3.onClick.AddListener(NoB3);
 		}
 
 	}
@@ -74,55 +200,40 @@ public class RandomTextUI : MonoBehaviour {
 			{
 				NoBut1.SetActive (false);
 				DSOOobject.SetActive (false);
+				change = true;
 			}
-			
+
+		
 	}
+
 	public void NoB2()
 	{
 			DSOO = true;
 			if(DSOO == true)
 			{
-			NoBut1.SetActive (false);
-			DSOOobject.SetActive (false);
+				NoBut2.SetActive (false);
+				DSOOobject.SetActive (false);
 			}
+			
 	}
+
 	public void NoB3()
 	{
 			DSOO = true;
 			if(DSOO == true)
 			{
-			NoBut1.SetActive (false);
-			DSOOobject.SetActive (false);
+				NoBut3.SetActive (false);
+				DSOOobject.SetActive (false);
 			}
+			
+	}
+	
+
+	
+
+	public void Si_B1()
+	{
+		SceneManager.LoadScene(Next);
 	}
 
-	public void ONoB1()
-	{
-			DSOO = true;
-			if(DSOO == true)
-			{
-			NoBut2.SetActive (false);
-			DSOOobject.SetActive (false);
-			}
-	}
-
-	public void ONoB2()
-	{
-			DSOO = true;
-			if(DSOO == true)
-			{
-			NoBut2.SetActive (false);
-			DSOOobject.SetActive (false);
-			}
-	}
-
-	public void ONoB3()
-	{
-			DSOO = true;
-			if(DSOO == true)
-			{
-			NoBut2.SetActive (false);
-			DSOOobject.SetActive (false);
-			}
-	}
 }
